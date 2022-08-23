869ab4bf2dd0c43064538b71b27dfebe67ea3cc3
abelardom3 <abelardom1999@gmail.com>
2022-08-23 11:56:33 -0400

    added twoSum

--- splitter.js
+++ splitter.js
@@ -5,16 +5,65 @@
 
 // notes:
 
-    // Subarrays cannot have a length of > num
-    // sub array CAN have a length that is <= num
+// Subarrays cannot have a length of > num
+// sub array CAN have a length that is <= num
+
+// function splitter(arr, num) {
+//     // your code here
+
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const num = 3
+
+// // should return [[1,2,3], [4,5,6], [7,8,9], [10]]
+
+// console.log(splitter(arr, num))
+
+
 
 function splitter(arr, num) {
     // your code here
+    var array = [];
+    var subArray = [];
+    var index = 1;
+
+    for (var i = 0; i < arr.length; i++) {
+        var current = arr[i]
+        // console.log(current)
+        while (current <= num) {
+            subArray.push(current);
+            current++
+        }
+
+        array.push(subArray)
+        return array
+
+    }
+
 }
 
-const arr = [1,2,3,4,5,6,7,8,9,10]
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const num = 3
 
 // should return [[1,2,3], [4,5,6], [7,8,9], [10]]
 
 console.log(splitter(arr, num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
